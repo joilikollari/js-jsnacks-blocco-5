21532ef2b9a1de5c9f94ddeaf1b3baf05204bfe2
joilikollari <likollari.giorgina@gmail.com>
2025-01-27 19:38:23 +0100

    add.exercise finished

---
+++ ex-js-array-methods-main/snack-2/script.js
@@ -0,0 +1,11 @@
+const people = [
+  { name: 'Paolo', age: 35 },
+  { name: 'Giulia', age: 24 },
+  { name: 'Marco', age: 67 }
+];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Paolo', 'Giulia', 'Marco'
+
+console.log(people.map((person) => person.name));
+
