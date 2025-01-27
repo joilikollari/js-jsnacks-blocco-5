aba98d643223aad51d06ed4f53baa94e5e719afa
joilikollari <likollari.giorgina@gmail.com>
2025-01-27 19:55:16 +0100

    add.change

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -3,5 +3,8 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 
-console.log(names);
+names.forEach(name => console.log(name));
+
+
+
 
